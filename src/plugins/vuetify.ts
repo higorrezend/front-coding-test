ed4b21fc370d7ba452931940f59bba789459ffc2
Higor Rezende <higorrezend@gmail.com>
2022-11-21 20:30:08 -0300

    feat: install and configure Vuetify

---
+++ src/plugins/vuetify.ts
@@ -0,0 +1,27 @@
+import Vue from 'vue'
+import pt from 'vuetify/src/locale/pt'
+import Vuetify from 'vuetify/lib/framework'
+
+Vue.use(Vuetify)
+
+export default new Vuetify({
+  lang: {
+    locales: {
+      pt
+    },
+    current: 'pt'
+  },
+  icons: {
+    iconfont: 'md'
+  },
+  theme: {
+    themes: {
+      light: {
+        primary: '#ED6161',
+        secondary: '#F5ECE3',
+        accent: '#EF6060',
+        error: '#b71c1c'
+      }
+    }
+  }
+})
