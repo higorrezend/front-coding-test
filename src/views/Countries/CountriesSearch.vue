9d817e05e1fe8b439dc1255e27037ee7bc0fd3ff
Higor Rezende <higorrezend@gmail.com>
2022-11-21 23:04:42 -0300

    feat: change CountriesSearch title.

--- src/views/Countries/CountriesSearch.vue
+++ src/views/Countries/CountriesSearch.vue
@@ -1,9 +1,11 @@
 <template>
-  <v-card class="countries-search elevation-1">
+  <v-card class="countries-search elevation-1 pa-2">
+    <h2 class="secondary--text mt-5 d-flex justify-center">
+      Filtrar dados sobre um país
+    </h2>
     <v-card-text>
       <v-row>
         <v-col col="12" sm="12" md="10" lg="10" xl="10" offset-md="1" offset-lg="1" offset-xl="1">
-          <h1 class="pb-5 mt-7">Filtrar dados sobre um país</h1>
           <v-text-field
             prepend-inner-icon="search"
             color="primary"
