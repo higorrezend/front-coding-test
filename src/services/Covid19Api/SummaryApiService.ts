fbc123213d313f4c53b038efb082e70e519b4cef
Higor Rezende <higorrezend@gmail.com>
2022-11-23 02:04:34 -0300

    fix: fixing URI endpoint in summary service.

--- src/services/Covid19Api/SummaryApiService.ts
+++ src/services/Covid19Api/SummaryApiService.ts
@@ -2,7 +2,7 @@ import { Summary } from '@/types/SummaryTypes'
 import { Covid19ApiService } from './Covid19ApiService'
 
 export class SummaryApiService extends Covid19ApiService {
-  protected uri = '/summaryyy'
+  protected uri = '/summary'
 
   public async getSummary (): Promise<Summary> {
     return await this.instance.get<Summary>(this.uri)
