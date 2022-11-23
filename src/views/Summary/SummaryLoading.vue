305b77547b934c9c65aec2d31000ee564bb415e7
Higor Rezende <higorrezend@gmail.com>
2022-11-23 15:59:52 -0300

    feat: adding loading on the summary view.

---
+++ src/views/Summary/SummaryLoading.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-card class="summary-loading elevation-0 mt-15" color="transparent">
+    <v-progress-circular
+      color="primary"
+      indeterminate
+      size="100"
+    ></v-progress-circular>
+    <h1 class="secondary--text mt-10 d-flex justify-center">
+      Buscando lista de países, por favor aguarde...
+    </h1>
+  </v-card>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'SummaryLoading'
+})
+</script>
+
+<style scoped>
+  .summary-loading {
+    text-align: center;
+  }
+</style>
--- src/views/Summary/index.vue
+++ src/views/Summary/index.vue
@@ -10,8 +10,18 @@
           a pandemia da COVID-19 ao redor do mundo.
         </h5>
       </v-col>
-      </v-row>
-      <v-row no-gutters class="mb-15">
+    </v-row>
+    <v-row v-if="loading">
+      <v-col cols="12">
+        <SummaryLoading/>
+      </v-col>
+    </v-row>
+    <v-row v-else-if="error.status">
+      <v-col cols="12">
+        <SummaryError/>
+      </v-col>
+    </v-row>
+    <v-row v-else no-gutters class="mb-15">
       <v-col cols="12" sm="12" md="8" lg="8" xl="8" offset-md="2" offset-lg="2" offset-xl="2" class="pt-10 mt-10">
         <SummaryCountriesSearch></SummaryCountriesSearch>
       </v-col>
@@ -24,19 +34,26 @@
 
 <script lang="ts">
 import Vue from 'vue'
-import { mapActions } from 'vuex'
+import { mapState, mapActions } from 'vuex'
+import SummaryError from './SummaryError.vue'
+import SummaryLoading from './SummaryLoading.vue'
 import SummaryCountriesList from './SummaryCountriesList.vue'
 import SummaryCountriesSearch from './SummaryCountriesSearch.vue'
 
 export default Vue.extend({
   name: 'Summary',
+  computed: {
+    ...mapState('Summary', ['loading', 'error'])
+  },
   methods: {
-    ...mapActions('Summary', ['getSumaryDataFromApi'])
+    ...mapActions('Summary', ['getSummaryDataFromApi'])
   },
   mounted () {
-    this.getSumaryDataFromApi()
+    this.getSummaryDataFromApi()
   },
   components: {
+    SummaryError,
+    SummaryLoading,
     SummaryCountriesList,
     SummaryCountriesSearch
   }
