6ea1177449b3626c25cc2159c897dc13e8b20a6e
Higor Rezende <higorrezend@gmail.com>
2022-11-24 13:00:00 -0300

    feat: changing the way of accessing the country status view to stay as requested.

--- src/views/Summary/index.vue
+++ src/views/Summary/index.vue
@@ -2,10 +2,10 @@
   <v-container class="summary">
     <v-row no-gutters>
       <v-col cols="12" sm="12" md="6" lg="6" xl="6">
-        <h1 class="summary__title secondary--text">
+        <h1 id="summary__title" class="summary__title secondary--text">
           Conheça o Covidômetro
         </h1>
-        <h5 class="summary__sub-title secondary--text">
+        <h5 id="summary__sub-title" class="summary__sub-title secondary--text">
           Fique atualizado com velocidade e transparência.
           O Covidômetro é uma ferramenta que mostra para você
           em tempo real o número de casos e óbitos relacionados
